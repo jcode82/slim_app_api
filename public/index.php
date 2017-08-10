3fba8634d258dda540285555496fa417d654df77
Juan Flores <jflifestyles@gmail.com>
2017-08-10 19:17:53 -0400

    require pdo connection to database on entry, hence gitignore db file

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
+require '../src/config/database.php';
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
