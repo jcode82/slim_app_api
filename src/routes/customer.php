ce8fac10916a32136f068ff6709e83e3beaab4cc
Juan Flores <jflifestyles@gmail.com>
2017-08-14 18:24:00 -0400

    add ability to add a new customer

--- src/routes/customer.php
+++ src/routes/customer.php
@@ -55,3 +55,44 @@ $app->get('/api/customer/{id}', function(Request $request, Response $response){
         echo '{"error": {"text": '.$e->getMessage().'}';
     }
 });
+
+//Add a customer
+$app->post('/api/customer/add', function(Request $request, Response $response){
+    
+        //get params passed in the url
+        $first_name = $request->getParam('first_name');
+        $last_name = $request->getParam('last_name');
+        $phone = $request->getParam('phone');
+        $email = $request->getParam('email');
+        $address = $request->getParam('address');
+        $city = $request->getParam('city');
+        $state = $request->getParam('state');
+    
+        $sql = "INSERT INTO customers (first_name,last_name,phone,email,address,city,state) VALUES 
+        (:first_name,:last_name,:phone,:email,:address,:city,:state)";
+        
+        try{
+            //get db Object
+            $db = new db();
+            //connect
+            $db = $db->connect();
+    
+            $stmt = $db->prepare($sql);
+
+            //bind placeholders for the sql stmt with the variable set to url params
+            $stmt->bindParam(':first_name', $first_name);
+            $stmt->bindParam(':last_name', $last_name);
+            $stmt->bindParam(':phone', $phone);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':address', $address);
+            $stmt->bindParam(':city', $city);
+            $stmt->bindParam(':state', $state);
+
+            $stmt->execute();
+
+            echo '{"notice": {"text": "Customer Added Successfully"}';
+    
+        } catch(PDOException $e){
+            echo '{"error": {"text": '.$e->getMessage().'}';
+        }
+    });
