73aa4c06202d11ad395f0cd5610ac46b4b6cd4c7
Juan Flores <jflifestyles@gmail.com>
2017-08-14 14:11:58 -0400

    create try catch get() in customers clean up that file, also clean up the index file to only have requires, and bug database typo

--- public/index.php
+++ public/index.php
@@ -1,17 +1,9 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+// use \Psr\Http\Message\ServerRequestInterface as Request;
+// use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 require '../src/config/database.php';
-
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
 
 //customer routes 
 require '../src/routes/customer.php';
--- src/routes/customer.php
+++ src/routes/customer.php
@@ -2,9 +2,31 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
+//get all customers
 $app = new \Slim\App;
 
-//get all customers
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
 $app->get('/api/customers', function(Request $request, Response $response){
-    echo 'CUSTOMERS';
+    $sql = "SELECT * FROM customers";
+    
+    try{
+        //get db Object
+        $db = new db();
+        //connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customers);
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
 });
