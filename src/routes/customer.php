075b9d835cdf5f041abee330d8d668429068c273
Juan Flores <jflifestyles@gmail.com>
2017-08-14 17:11:24 -0400

    create get single customer

--- src/routes/customer.php
+++ src/routes/customer.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-//get all customers
+//Hello name write test
 $app = new \Slim\App;
 
 $app->get('/hello/{name}', function (Request $request, Response $response) {
@@ -12,6 +12,7 @@ $app->get('/hello/{name}', function (Request $request, Response $response) {
     return $response;
 });
 
+//get all customers
 $app->get('/api/customers', function(Request $request, Response $response){
     $sql = "SELECT * FROM customers";
     
@@ -25,6 +26,30 @@ $app->get('/api/customers', function(Request $request, Response $response){
         $customers = $stmt->fetchAll(PDO::FETCH_OBJ);
         $db = null;
         echo json_encode($customers);
+
+    } catch(PDOException $e){
+        echo '{"error": {"text": '.$e->getMessage().'}';
+    }
+});
+
+//get single customer
+$app->get('/api/customer/{id}', function(Request $request, Response $response){
+
+    //get id passed in the url
+    $id = $request->getAttribute('id');
+
+    $sql = "SELECT * FROM customers WHERE id = $id";
+    
+    try{
+        //get db Object
+        $db = new db();
+        //connect
+        $db = $db->connect();
+
+        $stmt = $db->query($sql);
+        $customer = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $db = null;
+        echo json_encode($customer);
 
     } catch(PDOException $e){
         echo '{"error": {"text": '.$e->getMessage().'}';
